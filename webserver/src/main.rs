4c0d3905fdfe9230d5b628e5bfbbed4cd5c3e64e
julian <julianMBP>
2020-06-05 23:57:58 +0200

    restructured main

--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -2,63 +2,24 @@ use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::path::Path;
-use crate::threadpool::ThreadPool;
 
-
-const IP: &str = "localhost";
-const PORT: u32 = 8080;
-
-
+mod cli;
 mod file_util;
-mod threadpool;
+mod server;
 
 fn main() {
-    let address = format!("{}:{}", IP, PORT);
-    let listener = TcpListener::bind(address).unwrap();
+    let (address, directory, thread_pool_size) = cli::start_cli();
 
-    let pool = match ThreadPool::new(8) {
-        Err(err) => { panic!(err) }
-        Ok(pool) => pool
+    let _directory = load_directory(directory);
+
+    server::start_server(address.as_str(), thread_pool_size as usize);
+}
+
+
+// todo finish loading of directory in memory and returning it
+fn load_directory(path: String) {
+    match file_util::get_all_files_in_dir(Path::new(path.as_str())) {
+        Err(err) => { println!("{:?}", err) }
+        Ok(list) => println!("{:#?}", list)
     };
-
-    // match file_util::get_all_files_in_dir(Path::new("dist")) {
-    //     Err(err) => { println!("{:?}", err) }
-    //     Ok(list) => println!("{:#?}", list)
-    // };
-
-
-    println!("Listening for incoming requests on {}:{}", IP, PORT);
-    for stream in listener.incoming() {
-        let connection = stream.unwrap();
-        pool.execute(|| {
-            handle_connection(connection);
-        });
-    }
-}
-
-fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 512];
-    let response = "HTTP/1.1 200 OK\r\n\r\nhello";
-
-    stream.read(&mut buffer).unwrap();
-    println!("Request came in,sending response {}", response);
-
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
-}
-
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_add() {
-        assert_eq!(add(1, 2), 3);
-    }
-
-    #[test]
-    fn get_all_files_in_dir() {
-        assert_eq!(bad_add(1, 2), 3);
-    }
 }
