a9f78256994f0bc6048217f1ddced98f87933bf8
julian <julianMBP>
2020-06-21 16:03:13 +0200

    refactored servers to separate package

---
+++ server/src/hyper_server/mod.rs
@@ -0,0 +1,32 @@
+use std::convert::Infallible;
+use std::net::SocketAddr;
+use hyper::{Body, Request, Response, Server};
+use hyper::service::{make_service_fn, service_fn};
+use std::sync::Arc;
+use std::collections::HashMap;
+
+#[tokio::main]
+pub async fn start_server(ip: String, port: i32, dir: Arc<HashMap<String, String>>) {
+
+    let address = format!("{}:{}", ip,port);
+    let address: SocketAddr = address.parse().unwrap(); //correct ip format was handled before
+
+    let make_svc = make_service_fn(|_conn| async {
+        let function = service_fn(hello_world);
+        Ok::<_, Infallible>(function)
+    });
+
+    println!("Hyper server listening for incoming requests on {}", address);
+
+    let server = Server::bind(&address).serve(make_svc);
+
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
+}
+
+async fn hello_world(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    let uri = req.uri();
+
+    Ok(Response::new("Hello, World".into()))
+}
