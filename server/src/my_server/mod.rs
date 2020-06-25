fced4241eab05dccdfffcacda94ed8ca33d77c5b
julian <julianMBP>
2020-06-25 12:39:54 +0200

    Added response lie requests

--- server/src/my_server/mod.rs
+++ server/src/my_server/mod.rs
@@ -2,11 +2,13 @@ use std::collections::HashMap;
 use std::sync::Arc;
 use std::net::{TcpListener, TcpStream};
 use server::ThreadPool;
-use crate::my_server::requests::Request;
+use crate::my_server::request::{Request};
 use std::io::{Read, Write};
+use crate::my_server::response::Response;
 
 mod server;
-mod requests;
+mod request;
+mod response;
 
 /// Starts the server listening on the address,
 /// with the amount of threads provided by thread_pool_size.
@@ -42,38 +44,41 @@ pub fn start_server(ip: String, port: i32, thread_pool_size: i32, dir: Arc<HashM
 
 fn handle_connection(mut stream: TcpStream, dir: Arc<HashMap<String, Vec<u8>>>) {
     let mut buffer = [0; 2048];
-    let mut response200 = "HTTP/1.1 200 OK\r\n\r\n".as_bytes().to_vec();
-    let response404 = "HTTP/1.1 404 Not found\r\n\r\n".as_bytes().to_vec();
 
+    let mut response = Response::default_ok();
     stream.read(&mut buffer).unwrap();
-    let buffer = String::from_utf8(buffer.to_vec());
-    let buffer = match buffer {
-        Ok(value) => value,
-        Err(err) => {
-            println!("Not valid request: {}", err);
-            return;
-        }
+    let buffer = String::from_utf8(buffer.to_vec()); //todo https
+
+    if buffer.is_err() {
+        println!("Response could not be interpreted as string");
+        send_response(stream, Response::default_bad_request());
+        return;
     };
 
-    let request = Request::read_request(&buffer);
-
+    let request = Request::read_request(&buffer.unwrap());
     println!("Request came in, request: {:#?}", request);
-    let path = match request {
-        Ok(request) => request.request_identifiers.path,
-        Err(err) => {
-            println!("{}", err);
-            return;
-        }
+
+    if request.is_err() {
+        println!("Response could not be interpreted as string");
+        send_response(stream, Response::default_bad_request());
+        return;
     };
 
-    let response = match dir.get(&path) {
-        Some(value) => {
-            response200.append(&mut value.clone());
-            response200
-        }
-        None => response404
-    };
+    let key = request.unwrap().request_identifiers.path;
 
-    stream.write(&response).unwrap();
+    if !dir.contains_key(&key) {
+        println!("Requested source could not be found");
+        send_response(stream, Response::default_not_found());
+        return;
+    }
+
+    let resource = dir.get(&key).unwrap();
+    response.add_content_type(key);
+    response.body = resource.clone();
+    send_response(stream, response);
+}
+
+fn send_response(mut stream: TcpStream, mut response: Response) {
+    stream.write(&response.make_sendable()).unwrap();
     stream.flush().unwrap();
 }
