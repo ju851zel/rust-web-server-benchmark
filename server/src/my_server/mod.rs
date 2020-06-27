ab7ba3a48700ce6fb9413ea51a40ab3a47204f3c
Jörg Stenger <joergstenger@exozet.com>
2020-06-27 20:57:27 +0200

    WIP: returning ServerStats as JSON

--- server/src/my_server/mod.rs
+++ server/src/my_server/mod.rs
@@ -4,6 +4,10 @@ use std::net::{TcpListener, TcpStream};
 use server::ThreadPool;
 use crate::my_server::requests::Request;
 use std::io::{Read, Write};
+use std::str::FromStr;
+use std::any::Any;
+use std::convert::TryInto;
+use std::panic::resume_unwind;
 
 mod server;
 mod requests;
@@ -66,14 +70,60 @@ fn handle_connection(mut stream: TcpStream, dir: Arc<HashMap<String, Vec<u8>>>) 
         }
     };
 
-    let response = match dir.get(&path) {
-        Some(value) => {
-            response200.append(&mut value.clone());
-            response200
+//    let response = match dir.get(&path) {
+//        Some(value) => {
+//            response200.append(&mut value.clone());
+//            response200.clone()
+//        }
+//        None => response404.clone()
+//    };
+
+    let stats = String::from("/stats");
+
+    let mut response = match path {
+        stats => {
+            println!("gregregregr");
+            "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n".as_bytes().to_vec()
         }
-        None => response404
+        _ => response404
     };
+
+    let timber_resources: HashMap<&str, Vec<u8>> = [("Norway", "{\"name\": \"a\"}".as_bytes().to_vec())].iter().cloned().collect();
+    let timber_resources2: HashMap<&str, &str> = [("Norway", "gre")].iter().cloned().collect();
+
+
+
+    let a = [(1, 2)];
+    // <&str, &mut vec<u8>
+    let stats:  ServerStats = ServerStats { responses: [(200, 2), (404, 1)].iter().cloned().collect() };
+    // parse_to_json(stats);
+
+    response.append(&mut parse_to_json(stats).as_bytes().to_vec());
+
+    // println!("{}", parse_to_json(stats));
 
     stream.write(&response).unwrap();
     stream.flush().unwrap();
 }
+
+struct ServerStats {
+    responses: HashMap<i32, i32>
+}
+
+fn parse_to_json(stats: ServerStats) -> String {
+    let mut result_json = "{".to_string();
+
+    stats.responses.iter()
+        .for_each(|x| {
+            result_json = format!("{}\"{}\": \"{}\",", result_json, x.0, x.1);
+        });
+
+    if result_json.ends_with(",") {
+        result_json = format!("{}", &result_json[0..result_json.len()-1]);
+    }
+
+    let mut result_json = format!("{}{}", result_json, "}");
+
+    result_json
+}
+
--- server/src/rouille/mod.rs
+++ server/src/rouille/mod.rs
@@ -1,14 +1,18 @@
 use rouille::{Response, ResponseBody};
 use std::collections::HashMap;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
+use std::ops::Deref;
 
 pub fn start_server(ip: String, port: i32, dir: Arc<HashMap<String, Vec<u8>>>) {
     let address = format!("{}:{}", ip, port);
 
     println!("Rouille listening for incoming requests on {}", address);
 
+    let requests_counter = Mutex::new(0);
+
     rouille::start_server(address, move |request| {
         println!("{:#?}", request.url());
+        *requests_counter.lock().unwrap() += 1;
 
         let result = match dir.get(request.url().as_str()) {
             Some(value) => (200, value.to_vec()),
