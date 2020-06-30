f8a5aa21ff0d7a4f6647986553e6cf7654090d86
julian <julianMBP>
2020-06-30 23:46:37 +0200

    Code refactoring and cleanup

--- server/src/threaded/mod.rs
+++ server/src/threaded/mod.rs
@@ -7,31 +7,36 @@ use crate::response::Response;
 use crate::request::Request;
 
 mod server;
+
 /// Starts the server listening on the address,
 /// with the amount of threads provided by thread_pool_size.
 pub fn start_server(ip: String, port: i32, thread_pool_size: i32, dir: Arc<HashMap<String, Vec<u8>>>) {
     let pool = match ThreadPool::new(thread_pool_size as usize) {
         Ok(pool) => pool,
-        Err(err) => panic!(err)
+        Err(err) => {
+            println!("Threaded: Thread pool creation error. Consider a restart of the programm");
+            return;
+        }
     };
 
     let address = format!("{}:{}", ip, port);
 
-    println!("My server listening for incoming requests on {}", address);
-
     let listener = match TcpListener::bind(address) {
         Ok(listener) => listener,
-        Err(err) => panic!(err)
+        Err(err) => {
+            println!("Threaded: TCP bind error. Consider a restart of the programm");
+            return;
+        }
     };
     for stream in listener.incoming() {
-        println!("{:#?}", stream);
         let connection = match stream {
             Ok(stream) => stream,
             Err(e) => {
-                println!("{:#?}", e);
+                println!("Threaded: Connection error. Ignoring request");
                 continue;
             }
         };
+
         let dir = dir.clone();
         pool.execute(|| {
             handle_connection(connection, dir);
@@ -41,38 +46,40 @@ pub fn start_server(ip: String, port: i32, thread_pool_size: i32, dir: Arc<HashM
 
 fn handle_connection(mut stream: TcpStream, dir: Arc<HashMap<String, Vec<u8>>>) {
     let mut buffer = [0; 2048];
-
     let mut response = Response::default_ok();
-    stream.read(&mut buffer).unwrap();
-    let buffer = String::from_utf8(buffer.to_vec()); //todo https
 
-    if buffer.is_err() {
-        println!("Response could not be interpreted as string");
-        send_response(stream, Response::default_bad_request());
-        return;
-    };
-
-    let request = Request::read_request(&buffer.unwrap());
-    println!("Request came in, request: {:#?}", request);
-
-    if request.is_err() {
-        println!("Response could not be interpreted as string");
-        send_response(stream, Response::default_bad_request());
-        return;
-    };
-
-    let key = request.unwrap().request_identifiers.path;
-
-    if !dir.contains_key(&key) {
-        println!("Requested source could not be found");
-        send_response(stream, Response::default_not_found());
+    if let Err(err) = stream.read(&mut buffer).is_err() {
+        println!("Threaded: Error while processing request. Ignoring request");
         return;
     }
 
-    let resource = dir.get(&key).unwrap();
-    response.add_content_type(key);
-    response.body = resource.clone();
-    send_response(stream, response);
+    if let Err(err) = String::from_utf8(buffer.to_vec()) {
+        println!("Threaded: Request could not be interpreted as string.");
+        send_response(stream, Response::default_bad_request());
+        return;
+    }
+
+    let key = match Request::read_request(&buffer.unwrap()) {
+        Ok(request) => request.request_identifiers.path,
+        Err(err) => {
+            println!("Threaded: Request could not be interpreted as string.");
+            send_response(stream, Response::default_bad_request());
+            return;
+        }
+    };
+
+    match dir.get(&key) {
+        Some(resource) => {
+            response.add_content_type(key);
+            response.body = resource.clone();
+            send_response(stream, response);
+        }
+        None => {
+            println!("Threaded: Requested resource could not be found.");
+            send_response(stream, Response::default_not_found());
+            return;
+        }
+    }
 }
 
 fn send_response(mut stream: TcpStream, mut response: Response) {
