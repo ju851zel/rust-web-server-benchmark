b8359415348035b1badfdc2b597a34073ac09846
julian <julianMBP>
2020-06-30 23:57:14 +0200

    Code refactoring and cleanup in rouille

--- server/src/rouille/mod.rs
+++ server/src/rouille/mod.rs
@@ -5,10 +5,7 @@ use std::sync::Arc;
 pub fn start_server(ip: String, port: i32, dir: Arc<HashMap<String, Vec<u8>>>) {
     let address = format!("{}:{}", ip, port);
 
-    println!("Rouille listening for incoming requests on {}", address);
-
     rouille::start_server(address, move |request| {
-        println!("{:#?}", request.url());
 
         let result = match dir.get(request.url().as_str()) {
             Some(value) => (200, value.to_vec()),
