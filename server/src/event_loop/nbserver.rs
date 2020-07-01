ef7dda923b4defdc759d78812420b57171a5d312
julian <julianMBP>
2020-07-01 16:03:07 +0200

    formated code

--- server/src/event_loop/nbserver.rs
+++ server/src/event_loop/nbserver.rs
@@ -32,65 +32,77 @@ pub fn start_server(ip: String, port: i32, dir: Files) {
 
     loop {
         if incoming_q.events.len() > 0 {
-            let ready_listening_events = incoming_q.poll().unwrap();
-            for listen_event in ready_listening_events {
-                match listen_event.listener.accept() {
-                    Ok((stream, add)) => {
-                        let read_event = Event::new_read(stream, [0; 2048]);
-                        let worked = reading_q.add(read_event);
-                        if let Err(err) = worked {
-                            println!("Could not accept connection.");
-                        }
-                        let worked = incoming_q.add(listen_event);
-                        if let Err(err) = worked {
-                            panic!("Could not accept any more connections.")
-                        }
-                    }
-                    Err(err) => { println!("Could not accept connection."); }
-                };
-            }
+            handle_incoming(&mut incoming_q, &mut reading_q)
         }
         if reading_q.events.len() > 0 {
-            let ready_reading_events = reading_q.poll().unwrap();
-            for mut reading_event in ready_reading_events {
-                match reading_event.stream.read(&mut reading_event.data) {
-                    Err(error) => {
-                        println!("{}", error);
-                    }
-                    Ok(bytes_read) => {
-                        println!("I read {} bytes", bytes_read);
-                    }
-                };
-                let mut response = Request::create_response(reading_event.data, reading_q.dir.clone()).make_sendable();
-                let event = Event::new_write(reading_event.stream, from_slice(&response[..]));
-                let worked = writing_q.add(event);
-                if let Err(err) = worked {
-                    println!("Error while sending response")
-                }
-            }
+            handle_reading(&reading_q, &mut writing_q)
         }
         if writing_q.events.len() > 0 {
-            let ready_writing_events = writing_q.poll().unwrap();
-            for mut event in ready_writing_events {
-                let bytes_written = match event.stream.write(&mut event.data) {
-                    Err(error) if error.kind() == ErrorKind::WouldBlock => {
-                        println!("Would block");
-                        0
-                    }
-                    Err(error) => {
-                        println!("{}", error);
-                        0
-                    }
-                    Ok(bytes_read) => {
-                        println!("I wrote {} bytes", bytes_read);
-                        bytes_read
-                    }
-                };
-                if bytes_written != event.data.len() {
-                    println!("Not all written: buf: {}, written: {}", event.data.len(), bytes_written);
+            handle_writing(&mut writing_q)
+        }
+    }
+}
+
+fn handle_writing(writing_q: &mut Queue<Event>) {
+    let ready_writing_events = writing_q.poll().unwrap();
+    for mut event in ready_writing_events {
+        let bytes_written = match event.stream.write(&mut event.data) {
+            Err(error) if error.kind() == ErrorKind::WouldBlock => {
+                println!("Would block");
+                0
+            }
+            Err(error) => {
+                println!("{}", error);
+                0
+            }
+            Ok(bytes_read) => {
+                println!("I wrote {} bytes", bytes_read);
+                bytes_read
+            }
+        };
+        if bytes_written != event.data.len() {
+            println!("Not all written: buf: {}, written: {}", event.data.len(), bytes_written);
+        }
+    }
+}
+
+fn handle_reading(mut reading_q: &Queue<Event>, writing_q: &mut Queue<Event>) {
+    let ready_reading_events = reading_q.poll().unwrap();
+    for mut reading_event in ready_reading_events {
+        match reading_event.stream.read(&mut reading_event.data) {
+            Err(error) => {
+                println!("{}", error);
+            }
+            Ok(bytes_read) => {
+                // println!("I read {} bytes", bytes_read);
+            }
+        };
+        let mut response = Request::create_response(reading_event.data, reading_q.dir.clone()).make_sendable();
+        let event = Event::new_write(reading_event.stream, from_slice(&response[..]));
+        let worked = writing_q.add(event);
+        if let Err(err) = worked {
+            println!("Error while sending response")
+        }
+    }
+}
+
+fn handle_incoming(incoming_q: &mut Queue<ListenerEvent>, reading_q: &mut Queue<Event>) {
+    let ready_listening_events = incoming_q.poll().unwrap();
+    for listen_event in ready_listening_events {
+        match listen_event.listener.accept() {
+            Ok((stream, add)) => {
+                let read_event = Event::new_read(stream, [0; 2048]);
+                let worked = reading_q.add(read_event);
+                if let Err(err) = worked {
+                    println!("Could not accept connection.");
+                }
+                let worked = incoming_q.add(listen_event);
+                if let Err(err) = worked {
+                    panic!("Could not accept any more connections.")
                 }
             }
-        }
+            Err(err) => { println!("Could not accept connection."); }
+        };
     }
 }
 
