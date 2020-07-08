93dec1ea13105e6509a7b01584a5f95c02e82c31
julian <julianMBP>
2020-07-08 17:39:19 +0200

    added docs to request.rs and response.rs

--- server/src/response.rs
+++ server/src/response.rs
@@ -3,6 +3,7 @@ use std::path::Path;
 use std::ffi::OsStr;
 use std::fs;
 
+/// The object used in all the servers, to represent the http response.
 #[derive(Debug)]
 pub struct Response {
     pub response_identifiers: ResponseIdentifiers,
@@ -10,12 +11,18 @@ pub struct Response {
     pub body: Vec<u8>,
 }
 
+/// The identifier for the response, containing http method and version
+///
+/// E.g. Not_Found - 404 Http1.1
 #[derive(Debug, Eq, PartialEq)]
 pub struct ResponseIdentifiers {
     pub method: ResponseType,
     pub version: String,
 }
 
+/// The http response type
+///
+///E.g. Not_Found - 404
 #[derive(Debug, Eq, PartialEq)]
 pub struct ResponseType {
     name: String,
@@ -23,13 +30,17 @@ pub struct ResponseType {
 }
 
 impl ResponseType {
+    /// The default 200 - OK response
     fn ok() -> Self { Self { name: "OK".to_string(), id: 200 } }
+    /// The default 404 - Not Found response
     fn not_found() -> Self { Self { name: "Not Found".to_string(), id: 404 } }
+    /// The default 400 - Bad Request response
     fn bad_request() -> Self { Self { name: "Bad Request".to_string(), id: 400 } }
 }
 
 
 impl ResponseIdentifiers {
+    /// Makes the response identifyer into a sendable byte vector
     fn make_sendable(&self) -> Vec<u8> {
         let ident = format!("HTTP/{} {} {}\r\n", self.version, self.method.id, self.method.name);
         ident.as_bytes().to_vec()
@@ -37,6 +48,7 @@ impl ResponseIdentifiers {
 }
 
 impl Response {
+    /// Creates the default OK 200 response
     pub fn default_ok() -> Self {
         Self {
             response_identifiers: ResponseIdentifiers {
@@ -48,6 +60,7 @@ impl Response {
         }
     }
 
+    /// Creates the default Not Found 404 response
     pub fn default_not_found() -> Self {
         Self {
             response_identifiers: ResponseIdentifiers {
@@ -59,6 +72,7 @@ impl Response {
         }
     }
 
+    /// Creates the default Bad Request 400 response
     pub fn default_bad_request() -> Self {
         Self {
             response_identifiers: ResponseIdentifiers {
@@ -70,6 +84,7 @@ impl Response {
         }
     }
 
+    /// Creates a default error page response
     pub fn dynamic_error_response(&mut self, error_message: String) {
         let mut map = HashMap::new();
         let error_code: &str = &self.response_identifiers.method.id.to_string();
@@ -78,9 +93,10 @@ impl Response {
         map.insert("{{ErrorCode}}", error_code_full);
         map.insert("{{ErrorMessage}}", &error_message);
 
-        insert_dynamic_html(self, "..\\_dist\\error_page.html", map);
+        insert_dynamic_html(self, "_dist/error_page.html", map);
     }
 
+    /// Makes the response into a sendable byte vector
     pub fn make_sendable(&mut self) -> Vec<u8> {
         let mut ident = self.response_identifiers.make_sendable();
         let mut headers = self.make_headers_sendable();
@@ -92,9 +108,12 @@ impl Response {
         vec
     }
 
+    /// Adds a specific header into the response
     pub fn add_header(&mut self, header_key: &str, header_value: &str) {
         self.headers.insert(header_key.to_string(), header_value.to_string());
     }
+
+    /// Adds the content type into the response
     pub fn add_content_type(&mut self, file: String) {
         let file_ending = Path::new(&file).extension().and_then(OsStr::to_str).unwrap();
         let content_type = match file_ending {
@@ -110,6 +129,7 @@ impl Response {
         self.add_header("content-type",content_type);
     }
 
+    /// Makes the response header into a sendable byte vector
     pub fn make_headers_sendable(&self) -> Vec<u8> {
         let mut vec = Vec::with_capacity(self.headers.len() * 4 * 40);
         for pair in &self.headers {
@@ -122,6 +142,7 @@ impl Response {
     }
 }
 
+// todo comment what does it do after reimplementing the fs::read_string into correct
 pub fn insert_dynamic_html(mut response: &mut Response, path: &str, templating_replacements: HashMap<&str, &str>) {
     response.add_content_type("_.html".to_string());
     let mut resource = fs::read_to_string(path).unwrap();
